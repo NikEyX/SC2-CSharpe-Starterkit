e6ea620eec1fb50c7d1fb29ed5da7b8f95816e13
Travis <travisgleith@gmail.com>
2018-07-06 21:05:19 +0400

    changed map for testing, testing complete

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -14,7 +14,7 @@ namespace Bot
 //        private static string mapName = "AbyssalReefLE.SC2Map";
 //        private static string mapName = "AbiogenesisLE.SC2Map";
         
-        private static string mapName = "InterloperLE.SC2Map";
+        private static string mapName = "(2)16-BitLE.SC2Map";
 //        private static string mapName = "FrostLE.SC2Map";
         private static Race opponentRace = Race.Random;
         private static Difficulty opponentDifficulty = Difficulty.VeryEasy;
